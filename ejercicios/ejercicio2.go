b2e124ee556800bf01abdc9f07a91a07df7f2c3a
Benitez772 <alexanderivann77@gmail.com>
2025-05-22 19:50:42 -0300

    1

--- ejercicios/ejercicio2.go
+++ ejercicios/ejercicio2.go
@@ -1,14 +1,10 @@
 package ejercicios
 
-import "sort"
-
 type Tarea struct {
 	Nombre string
 	Tiempo int
 }
 
 func Ejercicio2(tareas []Tarea) {
-	sort.Slice(tareas, func(i, j int) bool {
-		return tareas[i].Tiempo < tareas[j].Tiempo
-	})
+	panic("No implementado")
 }
